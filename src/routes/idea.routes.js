d75ee83cefdcd111415ac5fcd3f0d47b4858375e
Daniel Aguilar <dyangeltk17@gmail.com>
2022-04-09 17:56:52 -0500

    adding pagination

--- src/routes/idea.routes.js
+++ src/routes/idea.routes.js
@@ -1,9 +1,11 @@
 const {Router} = require('express');
+const {AuthMiddleware,ParseIntMiddleware} = require('../middlewares');
+
 
 module.exports = function({IdeaController}){
   const router = Router();
 
-  router.get('', IdeaController.getAll); //only call reference function
+  router.get('', [AuthMiddleware, ParseIntMiddleware], IdeaController.getAll); //only call reference function
   router.get('/:ideaId', IdeaController.get); //only call reference function
   router.get('/:userId/all', IdeaController.getUserIdeas); //only call reference function
   router.post('', IdeaController.create); //only call reference function
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,11 @@
 const {Router} = require('express');
-const {AuthMiddleware} = require('../middlewares');
+const {AuthMiddleware,ParseIntMiddleware} = require('../middlewares');
 
 module.exports = function({UserController}){
   const router = Router();
 
   router.get('/:userId', UserController.get); //only call reference function
-  router.get('', [AuthMiddleware], UserController.getAll); //only call reference function
+  router.get('', [AuthMiddleware, ParseIntMiddleware], UserController.getAll); //only call reference function
   router.patch('/:userId', UserController.update); //only call reference function
   router.delete('/:userId', UserController.delete); //only call reference function
 
